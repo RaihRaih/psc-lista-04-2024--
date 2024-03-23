f457050cbc9b7169a58a6ab6b0f6cc82bebfb7cc
Raissa Rodrigues <105363135+RaihRaih@users.noreply.github.com>
2024-03-22 22:39:01 -0300

    Create Numeros.java

---
+++ Numeros.java
@@ -0,0 +1,19 @@
+/*Raissa Rodrigues - Sistemas de Informação
+ - questao 6 */
+
+public class Numeros {
+
+    public static void main(String[] args) {
+        // Imprime os números de 1 a 20 abaixo um do outro
+        System.out.println("Números de 1 a 20 abaixo um do outro:");
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+
+        // Imprime os números de 1 a 20 um ao lado do outro
+        System.out.println("\nNúmeros de 1 a 20 um ao lado do outro:");
+        for (int i = 1; i <= 20; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
